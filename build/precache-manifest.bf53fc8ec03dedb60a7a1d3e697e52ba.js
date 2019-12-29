865b9e2179955d9941a27bb86e3e6567a3aa4345
pulkit <nbt@nbts-MacBook-Pro-2.local>
2019-12-29 23:26:33 +0530

    heroku configs added and installed serve to serve the app

---
+++ build/precache-manifest.bf53fc8ec03dedb60a7a1d3e697e52ba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39db38470740e03ba059c3c16e8b04a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c7646999e798acdfc5",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "8b0c58731ce80b9020eb",
+    "url": "/static/css/main.57f0f91b.chunk.css"
+  },
+  {
+    "revision": "00c7646999e798acdfc5",
+    "url": "/static/js/2.78de055e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.78de055e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b0c58731ce80b9020eb",
+    "url": "/static/js/main.4322f872.chunk.js"
+  },
+  {
+    "revision": "d37b5d88045dc7b3cead",
+    "url": "/static/js/runtime-main.c38fda6a.js"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/static/media/Bitmap.7173cfe7.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/static/media/Bitmap.90640a87.png"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/static/media/Bitmap.977ecf7b.png"
+  },
+  {
+    "revision": "aff6dfc6a1b059d995aee89227bc9431",
+    "url": "/static/media/bluestacks_logo.aff6dfc6.png"
+  }
+]);
